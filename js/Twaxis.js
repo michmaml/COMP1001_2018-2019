ebf1f227d65573706578346b10b5c34ce5bc2b16
Jamie Huddlestone <jhuddlestone1@sheffield.ac.uk>
2019-03-05 16:50:06 +0000

    JavaScript functions

---
+++ js/Twaxis.js
@@ -0,0 +1,33 @@
+/* JavaScript functions for Twaxis web application
+ * COM1001 Spring Semester Assignment 2019
+ * Jamie Huddlestone
+ */
+
+var Twaxis = {
+	
+	// Generic method for AJAX calls; probably easier to use .get() or .post(), see below.
+	ajax: function (method, url, data, callback) {
+		
+		var request = new XMLHttpRequest;
+		request.open(method, url);
+		if (typeof data == "object") {
+			request.setRequestHeader("Content-Type", "application/json");
+			data = JSON.stringify(data);
+		}
+		request.responseType = "text";
+		request.addEventListener("load", callback);
+		request.send(data);
+		
+		return request;
+	},
+	
+	// Specific function for AJAX GET calls.
+	get: function (url, callback) {
+		return ajax("GET", url, null, callback);
+	},
+	
+	// Specific function to POST data with AJAX... use this!
+	post: function (url, data, callback) {
+		return ajax("POST", url, data, callback);
+	}
+}
