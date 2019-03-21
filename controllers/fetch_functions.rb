dc8417415ff213d7fbc5c0b87fc9984e25970c7c
Michal Sekulski <mjsekulski1@sheffield.ac.uk>
2019-03-21 12:24:29 +0000

    uploading orders to db, no rejcetion(yet)

--- controllers/fetch_functions.rb
+++ controllers/fetch_functions.rb
@@ -36,6 +36,38 @@ end
 
 #-------------------------------------------------------------------------------
 
+def upload_tweets_to_db # Michal
+
+get '/add' do
+    @submitted = false
+erb :add
+end
+
+post '/add' do
+    @submitted = true
+  # sanitize values
+      @date = order[:date].strip
+      @time = order[:time].strip
+      @from = order[:from].strip
+      @to = order[:to].strip
+     #@car = order[:car].strip
+     #@OrderID = 
+     @UserID ={user.screen_name} 
+    # get next available ID
+    id = @db.get_first_value 'SELECT MAX(id)+1 FROM Orders';
+
+    # do the insert
+    @db.execute(
+      'INSERT INTO cities VALUES (?, ?, ?, ?, ?)',
+      [id, @from, @date, @time, @to])
+    
+     #'INSERT INTO cities VALUES (?, ?, ?, ?, ?, ?)',
+     #[OrderID,@CarID,@UserID, @from, @date, @time])
+    end
+end
+
+#-------------------------------------------------------------------------------
+
 def fetch_tweets # Michal
 	
 	results = @twitter.search(SEARCH_STRING)
@@ -45,8 +77,13 @@ end
 
 #-------------------------------------------------------------------------------
 
-def fetch_users
-	
+def fetch_users # Huiqiang
+    user_id = params[:screen_name]
+    query = %{SELECT pickup_location, date, time 
+              From Orders
+              WHERE UserID = '%#{user_id}%'}
+    @results = @db.execute query, params[:search]
+
 end
 
 #-------------------------------------------------------------------------------
