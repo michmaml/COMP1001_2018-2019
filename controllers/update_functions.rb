e74fa0a8ce2f71c82fe49670bbb5af56c53f8281
Kacper Lodzinski <ktlodzinski1@sheffield.ac.uk>
2019-03-22 14:46:58 +0000

    update function added

--- controllers/update_functions.rb
+++ controllers/update_functions.rb
@@ -3,7 +3,11 @@
 #-------------------------------------------------------------------------------
 
 def update_order
-   
+   orderID = params[:in_reply_to_status_id].strip.to_i
+    @db.execute(
+			"UPDATE SET Status = 1 WHERE CustomerID = ?;",
+			[orderID])
+
 	
 end
 
