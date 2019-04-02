a88efa0e4388fa2c5eb179f9edd395d0c0bd5f0c
Ziting Zhao <zzhao30@sheffield.ac.uk>
2019-04-02 19:07:16 +0000

    cars-new

--- controllers/controllers/fetch_users.rb
+++
@@ -1,6 +0,0 @@
-# fetch_users.rb
-# <description here>
-# COM1001 Spring Semester Assignment 2019
-# <name here>
-
-
--- controllers/fetch_tweets.rb
+++
@@ -1,6 +0,0 @@
-# fetch_tweets.rb
-# <description here>
-# COM1001 Spring Semester Assignment 2019
-# <name here>
-
-
